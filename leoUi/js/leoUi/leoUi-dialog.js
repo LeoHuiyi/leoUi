265165befd03a6066e1118b0fd211864cb7f8dbd
leo <253383559@qq.com>
2014-07-22 13:03:15 +0800

    fix bug

--- leoUi/js/leoUi/leoUi-dialog.js
+++ leoUi/js/leoUi/leoUi-dialog.js
@@ -192,7 +192,7 @@
 
                 var zIndex = $(target).css('zIndex');
 
-                zIndex > 1 && zIndex = zIndex -1;
+                zIndex > 1 && ( zIndex = zIndex -1 );
 
                 return $('<div class="a" style = "position: fixed;top: 0px; left: 0px;width:100%;height:100%;background-color:black;overflow:hidden;z-index: '+ zIndex +';"></div>').hide().appendTo('body');
 

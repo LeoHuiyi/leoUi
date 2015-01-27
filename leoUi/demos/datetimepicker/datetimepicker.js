d995571fd04b83bc0336f66f279183271015fb29
leo <253383559@qq.com>
2015-01-27 17:28:13 +0800

    update

---
+++ leoUi/demos/datetimepicker/datetimepicker.js
@@ -0,0 +1,43 @@
+
+leoUiLoad.config({
+
+    debug: false,
+
+    baseUrl:'leoUi/',
+
+    alias : {
+
+        base: '../../css/base.css',
+
+        leoUi: '../../css/leoUi.css'
+
+    },
+
+    shim: {
+
+        jquery: {
+
+            src: '../jquery/jquery-1.9.1.js',
+
+            exports: "$"
+
+        }　,
+
+        dateZn: {
+
+            src: '../jquery/date.zn.min.js',
+
+            exports: "Date"
+
+        }
+
+    }
+
+});
+
+leoUiLoad.require('leoUi-datetimepicker,leoUi, ready', function($) {
+
+    
+
+});
+
